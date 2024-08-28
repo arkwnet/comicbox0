b97bc7b98c5a5d9a2029de34fc567391000250af
Sora Arakawa <mail@arkw.net>
2024-08-28 22:41:28 +0900

    ALPS-Rei-FE: Edit cart with key

--- ALPS-Rei-FE/ReiApplication.py
+++ ALPS-Rei-FE/ReiApplication.py
@@ -34,12 +34,39 @@ class Application(tkinter.Frame):
         total_price = 200
         self.image_bgr = self.draw_text(self.image_bgr, str(total_count).rjust(5), 1100, 436, self.font, (0, 0, 0))
         self.image_bgr = self.draw_text(self.image_bgr, str(total_price).rjust(5), 1100, 482, self.font, (0, 0, 0))
+        # 購入リスト
+        for i in range(len(self.cart)):
+            self.image_bgr = self.draw_text(self.image_bgr, str(i + 1), 25, 132 + i * 32, self.font_table, (0, 0, 0))
+            self.image_bgr = self.draw_text(self.image_bgr, self.cart[i].name, 145, 132 + i * 32, self.font_table, (0, 0, 0))
+            self.image_bgr = self.draw_text(self.image_bgr, str(self.cart[i].quantity).rjust(2
+            ), 530, 132 + i * 32, self.font_table, (0, 0, 0))
+            self.image_bgr = self.draw_text(self.image_bgr, str(self.cart[i].price).rjust(4), 615, 132 + i * 32, self.font_table, (0, 0, 0))
+            self.image_bgr = self.draw_text(self.image_bgr, str(self.cart[i].price * self.cart[i].quantity).rjust(5), 735, 132 + i * 32, self.font_table, (0, 0, 0))
+        # 商品リスト
+        for i in range(len(self.items)):
+            self.image_bgr[101 + i * 44:147 + i * 44, 815:1255] = self.image_item
+            self.image_bgr = self.draw_text(self.image_bgr, self.items[i].name, 865, 112 + i * 44, self.font_table, (0, 0, 0))
+            self.image_bgr = self.draw_text(self.image_bgr, self.keymap[i].full, 1215, 112 + i * 44, self.font_table, (0, 0, 0))
         # ウインドウに転送
         self.image_rgb = cv2.cvtColor(self.image_bgr, cv2.COLOR_BGR2RGB)
         self.image_pil = Image.fromarray(self.image_rgb)
         self.image_tk = ImageTk.PhotoImage(self.image_pil)
         self.canvas.create_image(0, 0, image = self.image_tk, anchor = "nw")
         self.after(100, self.loop)
+    
+    def key_event(self, e):
+        if e.keysym == "Escape":
+            self.cart.clear()
+        else:
+            for i in range(len(self.items)):
+                if e.keysym == self.keymap[i].half:
+                    hit = False
+                    for j in range(len(self.cart)):
+                        if self.cart[j].name == self.items[i].name:
+                            self.cart[j].quantity += 1
+                            hit = True
+                    if hit == False:
+                        self.cart.append(self.items[i])
     
     def __init__(self, master = None):
         super().__init__(master)
@@ -49,12 +76,25 @@ class Application(tkinter.Frame):
         self.canvas = tkinter.Canvas(self.master, highlightthickness = 0)
         self.canvas.place(x = 0, y = 0, w = ReiConfig.WIDTH, h = ReiConfig.HEIGHT)
         self.font_clock = ImageFont.truetype("./assets/Kosugi-Regular.ttf", 18)
+        self.font_table = ImageFont.truetype("./assets/Kosugi-Regular.ttf", 22)
         self.font = ImageFont.truetype("./assets/Kosugi-Regular.ttf", 42)
         self.image_main = cv2.imread("./assets/main.png")
+        self.image_item = cv2.imread("./assets/item.png")
+        self.keymap = [
+            ReiCommon.Key("Ｑ", "q"),
+            ReiCommon.Key("Ｗ", "w"),
+            ReiCommon.Key("Ｅ", "e"),
+            ReiCommon.Key("Ｒ", "r"),
+            ReiCommon.Key("Ｔ", "t"),
+            ReiCommon.Key("Ｙ", "y"),
+            ReiCommon.Key("Ｕ", "u")
+        ]
         self.items = []
+        self.cart = []
         with open("./items.csv", encoding = "utf-8") as f:
             for line in f:
                 line_array = line.split(",")
                 self.items.append(ReiCommon.Item(line_array[0], int(line_array[1]), 1))
+        self.master.bind("<KeyPress>", self.key_event)
         self.loop()
         self.master.mainloop()
--- ALPS-Rei-FE/ReiCommon.py
+++ ALPS-Rei-FE/ReiCommon.py
@@ -1,5 +1,17 @@
+class Display():
+    def __init__(self, upper_left, upper_right, lower_left, lower_right):
+        self.upper_left = upper_left
+        self.upper_right = upper_right
+        self.lower_left = lower_left
+        self.lower_right = lower_right
+
 class Item():
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
+
+class Key():
+    def __init__(self, full, half):
+        self.full = full
+        self.half = half
