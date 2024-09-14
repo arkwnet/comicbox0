59b3423577339b028b2211da1c04fe2e52f0f0f0
Sora Arakawa <mail@arkw.net>
2024-09-14 21:36:18 +0900

    ALPS-Rei-CP: Implement application

--- ALPS-Rei-CP/app/src/main/java/jp/arkw/alpsrei/cp/MainActivity.java
+++ ALPS-Rei-CP/app/src/main/java/jp/arkw/alpsrei/cp/MainActivity.java
@@ -1,8 +1,12 @@
 package jp.arkw.alpsrei.cp;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +18,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.sunmi.peripheral.printer.InnerPrinterCallback;
+import com.sunmi.peripheral.printer.InnerPrinterException;
+import com.sunmi.peripheral.printer.InnerPrinterManager;
+import com.sunmi.peripheral.printer.InnerResultCallback;
+import com.sunmi.peripheral.printer.SunmiPrinterService;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,19 +35,32 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private boolean running = false;
+    private boolean isClock = false;
+    private boolean isReceipt = false;
     private boolean permission = false;
-    private String url = "";
+    private String backend = "";
+    private int id = -1;
     private Timer timer;
-    private TimerTask timerTask;
-    private Task task;
+    private TimerTask timerTaskClock;
+    private TimerTask timerTaskReceipt;
+    private TaskClock taskClock;
+    private TaskReceipt taskReceipt;
     private TextView textViewClock;
     private TextView textViewUrl;
     private TextView textViewVersion;
+
+    private SunmiPrinterService sunmiPrinterService;
+    public static int NoSunmiPrinter = 0x00000000;
+    public static int CheckSunmiPrinter = 0x00000001;
+    public static int FoundSunmiPrinter = 0x00000002;
+    public static int LostSunmiPrinter = 0x00000003;
+    public int sunmiPrinter = CheckSunmiPrinter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,23 +72,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initSunmiPrinterService(this);
         findViewById(R.id.button_url).setOnClickListener(this);
+        findViewById(R.id.button_card).setOnClickListener(this);
         textViewClock = findViewById(R.id.text_view_clock);
         textViewUrl = findViewById(R.id.text_view_url);
         textViewVersion = findViewById(R.id.text_view_version);
         textViewVersion.setText("Version " + getResources().getString(R.string.versionName));
         timer = new Timer();
-        timerTask = new TimerTask() {
+        timerTaskClock = new TimerTask() {
             @Override
             public void run() {
-                if (running == false && permission == true && url.contains("http")) {
-                    running = true;
-                    task = new Task();
-                    task.execute(url + "/clock");
+                if (isClock == false && permission == true && backend.contains("http")) {
+                    isClock = true;
+                    taskClock = new TaskClock();
+                    taskClock.execute(backend + "/clock");
                 }
             }
         };
-        timer.schedule(timerTask, 0, 1000);
+        timer.schedule(timerTaskClock, 0, 1000);
+        timerTaskReceipt = new TimerTask() {
+            @Override
+            public void run() {
+                if (isReceipt == false && permission == true && backend.contains("http")) {
+                    isReceipt = true;
+                    taskReceipt = new TaskReceipt();
+                    taskReceipt.execute(backend + "/receipt");
+                }
+            }
+        };
+        timer.schedule(timerTaskReceipt, 0, 1000);
         permission = true;
     }
 
@@ -81,8 +118,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     .setPositiveButton(
                             "OK",
                             (dialog, which) -> {
-                                url = editText.getText().toString();
-                                textViewUrl.setText(url + "/");
+                                backend = editText.getText().toString();
+                                textViewUrl.setText(backend + "/");
                                 permission = true;
                             })
                     .setNegativeButton(
@@ -91,39 +128,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 permission = true;
                             })
                     .show();
+        } else if (v.getId() == R.id.button_card) {
+            printImage(BitmapFactory.decodeResource(getResources(), R.drawable.card));
+            feedPaper(5);
         }
     }
 
-    private class Task extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... urls) {
-            HttpURLConnection httpURLConnection;
-            InputStream inputStream;
-            String result;
-            String str = "";
-            try {
-                URL url = new URL(urls[0]);
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setConnectTimeout(10000);
-                httpURLConnection.setReadTimeout(10000);
-                httpURLConnection.setRequestMethod("GET");
-                httpURLConnection.connect();
-                int statusCode = httpURLConnection.getResponseCode();
-                if (statusCode == 200){
-                    inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+    private String call(String uri) {
+        HttpURLConnection httpURLConnection;
+        InputStream inputStream;
+        String result;
+        String str = "";
+        try {
+            URL url = new URL(uri);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(10000);
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+            int statusCode = httpURLConnection.getResponseCode();
+            if (statusCode == 200){
+                inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+                result = bufferedReader.readLine();
+                while (result != null){
+                    str += result;
                     result = bufferedReader.readLine();
-                    while (result != null){
-                        str += result;
-                        result = bufferedReader.readLine();
-                    }
-                    bufferedReader.close();
                 }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                bufferedReader.close();
             }
-            return str;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    private class TaskClock extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return call(urls[0]);
         }
 
         @Override
@@ -131,11 +175,199 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 JSONObject jsonObject = new JSONObject(str_recv);
                 textViewClock.setText(jsonObject.getString("clock"));
-                running = false;
+                isClock = false;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return;
+        }
+    }
+
+    private class TaskReceipt extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return call(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String str_recv) {
+            try {
+                JSONObject jsonObject = new JSONObject(str_recv);
+                int nid = Integer.parseInt(jsonObject.getString("id"));
+                if (id >= 0 && id != nid) {
+                    printImage(BitmapFactory.decodeResource(getResources(), R.drawable.receipt1));
+                    printText("COMIC BOX #0 / 海11\n", 0);
+                    printImage(BitmapFactory.decodeResource(getResources(), R.drawable.receipt2));
+                    printText("ご購入になりました商品の\n", 0);
+                    printText("サポート情報はこちら ↓\n", 0);
+                    printText("https://arkw.work/doujin\n", 0);
+                    Date date = new Date();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd(E) HH:mm");
+                    printText("レジ0001 " + simpleDateFormat.format(date) + "\n", 0);
+                    printText("取" + String.format("%04d", nid) + " 責: 01 荒川\n\n", 0);
+                    JSONArray items = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        final int price = Integer.parseInt(item.getString("price"));
+                        final int quantity = Integer.parseInt(item.getString("quantity"));
+                        printText("" + item.getString("name") + " × " + quantity + "\n", 0);
+                        final int subtotal = price * quantity;
+                        printText("" + subtotal + "\n", 2);
+                    }
+                    printLine();
+                    printDoubleText("合計", "￥ " + Integer.parseInt(jsonObject.getString("total")));
+                    printDoubleText(jsonObject.getString("payment").replaceAll("\\r\\n|\\r|\\n", ""), "￥ " + Integer.parseInt(jsonObject.getString("cash")));
+                    printDoubleText("お釣り", "￥ " + Integer.parseInt(jsonObject.getString("change")));
+                    printLine();
+                    printText("X/Twitter: @arkw0\n", 0);
+                    printText("Misskey: @arkw@mi.arkw.work\n", 0);
+                    printText("Website: https://arkw.net/\n", 0);
+                    printText("E-mail: mail@arkw.net", 0);
+                    feedPaper(5);
+                }
+                id = nid;
+                isReceipt = false;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void printText(String text, int alignment) {
+        try {
+            sunmiPrinterService.setAlignment(alignment, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+        try {
+            sunmiPrinterService.printText(text, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private void printDoubleText(String textLeft, String textRight) {
+        try {
+            sunmiPrinterService.printColumnsText(new String[]{textLeft, textRight}, new int[]{23, 8}, new int[]{0, 2}, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private void printImage(Bitmap bitmap) {
+        try {
+            sunmiPrinterService.printBitmap(bitmap, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private void printLine() {
+        printText("--------------------------------\n", 1);
+    }
+
+    private void feedPaper(int n) {
+        try {
+            sunmiPrinterService.lineWrap(n, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private InnerPrinterCallback innerPrinterCallback = new InnerPrinterCallback() {
+        @Override
+        protected void onConnected(SunmiPrinterService service) {
+            sunmiPrinterService = service;
+            checkSunmiPrinterService(service);
+        }
+        @Override
+        protected void onDisconnected() {
+            sunmiPrinterService = null;
+            sunmiPrinter = LostSunmiPrinter;
+        }
+    };
+
+    private void checkSunmiPrinterService(SunmiPrinterService service) {
+        boolean ret = false;
+        try {
+            ret = InnerPrinterManager.getInstance().hasPrinter(service);
+        } catch (InnerPrinterException e) {
+            e.printStackTrace();
+        }
+        sunmiPrinter = ret?FoundSunmiPrinter:NoSunmiPrinter;
+    }
+
+    public void initSunmiPrinterService(Context context) {
+        try {
+            boolean ret =  InnerPrinterManager.getInstance().bindService(context, innerPrinterCallback);
+            if (!ret) {
+                sunmiPrinter = NoSunmiPrinter;
+            }
+        } catch (InnerPrinterException e) {
+            e.printStackTrace();
         }
     }
 }
